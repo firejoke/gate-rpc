49873c4d8ef05a9358dc947f85867711672c0655
firejoke <38064902+firejoke@users.noreply.github.com>
2024-01-03 14:20:00 +0800

    build

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
+import gaterpc
 
 
 setup(
     name='gaterpc',
-    version='0.1.0',
+    version=gaterpc.__version__,
     packages=['gaterpc'],
     url='https://github.com/firejoke/gate-rpc',
     license='BSD-3-Clause',
-    author='shifan',
+    author='Shi Fan',
     author_email='firejokeshi@gmail.com',
     description='A RPC software based on ZeroMQ with built-in Majordomo.',
     classifiers=[
